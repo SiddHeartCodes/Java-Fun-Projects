951bb95b2b26a3b5584a45c058fb75ffc0b6e244
Siddharth Chakravarty <136830938+SiddHeartCodes@users.noreply.github.com>
2023-08-06 17:49:16 +0530

    Update Main.java
    Added more optimized backtracking algorithm

--- EightQueenProblem/src/Main.java
+++ EightQueenProblem/src/Main.java
@@ -10,50 +10,52 @@ public class Main
     static List<Queen> number=new ArrayList<>(); //To store the number of queens placed
     static Scanner scanner=new Scanner(System.in);
 
-    public static void main(String[] args)
-    {
-       checkPlace(number,board);
-        System.out.println(n/(8 * 7 * 6 * 5 * 4 * 3 * 2)); //We are dividing by 8! as there would be 8! ways to place
-        // same arrangement again
+    public static void main(String[] args) {
+        check(0);
+        System.out.println(n);
+
     }
 
-    public static void checkPlace(List<Queen> number, Queen[][] board) {
-        if (number.size() != 8) {
-            for (int i = 0; i < 8; i++) {
-                for (int j = 0; j < 8; j++) {
-                    int flag = 1;
-                    if(number.size()!=0)
-                    {
-                        for (Queen queen : number) {
-                            if (i == queen.getRow() || j == queen.getColumn()) {
-                                flag = 0;
-                                break;
-                            }
-                        }
-                    }
-                    if (flag == 0)
-                        continue;
-                    board[i][j] = new Queen(i, j);
-
-                    for (Queen queen : number) {
-                        if (!queen.canMove(board)) {
-                            flag = 0;
-                            break;
-                        }
-                    }
-                    if (flag == 0) {
-                        board[i][j] = null;
-                    } else {
-                        number.add(board[i][j]);
-                        checkPlace(number, board);// Recursively continue searching for other solutions
-                        number.remove(board[i][j]);
-                        board[i][j] = null;
-                    }
-                }
+    public static void check(int rowstart)
+    {
+        if(rowstart==0)
+        {
+            for(int i=0;i<8;i++)
+            {
+                board[rowstart][i]=new Queen(rowstart,i);
+                number.add(board[rowstart][i]);
+                check(rowstart+1);
+                number.remove(board[rowstart][i]);
+                board[rowstart][i]=null;
             }
-        } else {
+        }
+        else if(rowstart<8&&rowstart>0)
+        {
+            for(int k=0;k<8;k++) {
+                int flag=1;
+                board[rowstart][k]=new Queen(rowstart,k);
+                for (Queen queen : number)
+                {
+                    if (!queen.canMove(board))
+                    {
+                        flag=0;
+                        break;
+                        }
+                }
+                if (flag==1)
+                {
+                    number.add(board[rowstart][k]);
+                    check(rowstart+1);
+                    number.remove(board[rowstart][k]);
+                }
+                board[rowstart][k]=null;
+            }
+        }
+        else if(rowstart==8)
+        {
             n++;
         }
     }
+
 
 }
